5f7116b48904b956725c39d1c5016bb26745b5a8
Dylan Pulliam <dpulliam@customink.com>
2016-02-19 13:49:15 -0500

    Add some module updates to normalization

--- data/data_helper.rb
+++ data/data_helper.rb
@@ -1,33 +1,62 @@
 module DataHelper
+  module  Normalization
 
-  def DataHelper.normalize_ordinal (data_set, upper_bound, lower_bound)
-    num_data_points = data_set.length
-    normalized_data = Array.new
-    data_set.each_with_index do |data, index|
-      normalized_data[index] = (((upper_bound.to_f - lower_bound.to_f) * (index + 1).to_f) / num_data_points.to_f) + lower_bound.to_f
+    class Qualitative
+      class << self
+        def normalize_qualitative()
+        end
+
+        def normalize_ordinal (data_set, upper_bound, lower_bound)
+          num_data_points = data_set.length
+          normalized_data = Array.new
+          data_set.each_with_index do |data, index|
+            normalized_data[index] = (((upper_bound.to_f - lower_bound.to_f) * (index + 1).to_f) / num_data_points.to_f) + lower_bound.to_f
+          end
+          return normalized_data
+        end
+
+        def denormalize_ordinal(data_set, upper_bound, lower_bound)
+          num_data_points = data_set.length
+          normalized_data = Array.new
+
+          data_set.each_with_index do |data, index|
+            normalized_data[index] = ((num_data_points.to_f * (data.to_f - lower_bound.to_f ))/(upper_bound.to_f - lower_bound.to_f)) - 1
+          end
+          return normalized_data
+        end
+
+        def noralize_nominal(data_set, upper_bound, lower_bound)
+          self.one_of_n(data_set, upper_bound, lower_bound)
+        end
+
+        def denormalize_nominal
+        end
+
+        def self.one_of_n(data_set, upper_bound, lower_bound)
+          num_data_points = data_set.length
+          normalized_data = Hash.new
+          data_set.each_with_index do |data, index|
+            tmp = Array.new(num_data_points, lower_bound)
+            tmp[index] = upper_bound
+            normalized_data[data.to_sym] = tmp
+          end
+          return normalized_data
+        end
+      end
     end
-    return normalized_data
-  end
 
-  def DataHelper.normalize_nominal (data_set, upper_bound, lower_bound)
-    num_data_points = data_set.length
-    normalized_data = Hash.new
-    data_set.each_with_index do |data, index|
-      tmp = Array.new(num_data_points, lower_bound)
-      tmp[index] = upper_bound
-      normalized_data[data.to_sym] = tmp
+    class Quantitative
+      class << self
+        def normalize_ratio
+          self.normalize_quantitative()
+        end
+
+        def normalize_interval
+          self.normalize_quantitative()
+        end
+      end
     end
-    return normalized_data
-  end
-
-
-  def DataHelper.denormalize_ordinal
 
   end
-
-  def DataHelper.denormalize_nominal
-
-  end
-
 end
 
--- distance/distance.rb
+++ distance/distance.rb
@@ -1,31 +1,32 @@
-module Distance
+module DataHelper
+  class Distance
 
+    def Distance.euclidean (vector_a, vector_b)
+      sum = 0
+      vector_a.each_with_index { |data_point, index|
+        difference = data_point - vector_b[index]
+        sum = sum + difference**2
+      }
+      Math.sqrt(sum)
+    end
 
-  def Distance.euclidean (vector_a, vector_b)
-    sum = 0
-    vector_a.each_with_index { |data_point, index|
-      difference = data_point - vector_b[index]
-      sum = sum + difference**2
-    }
-    Math.sqrt(sum)
+    def Distance.manhattan (vector_a, vector_b)
+      sum = 0
+      vector_a.each_with_index { |data_point, index|
+        difference = (data_point - vector_b[index]).abs
+        sum += difference
+      }
+      sum
+    end
+
+    def Distance.chebyshev (vector_a, vector_b)
+      result = 0
+      vector_a.each_with_index { |data_point, index|
+        difference = (data_point - vector_b[index]).abs
+        result = [result, difference].max
+      }
+      result
+    end
+
   end
-
-  def Distance.manhattan (vector_a, vector_b)
-    sum = 0
-    vector_a.each_with_index { |data_point, index|
-      difference = (data_point - vector_b[index]).abs
-      sum += difference
-    }
-    sum
-  end
-
-  def Distance.chebyshev (vector_a, vector_b)
-    result = 0
-    vector_a.each_with_index { |data_point, index|
-      difference = (data_point - vector_b[index]).abs
-      result = [result, difference].max
-    }
-    result
-  end
-
 end
